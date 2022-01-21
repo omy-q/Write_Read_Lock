849f255b5599708b730e6f4a7e23b9f52f34eea1
omy-q <olya_aparina@mail.ru>
2022-01-21 21:02:35 +0300

    добавление поля lock для работы с многооточкой: 1. добавление блокировок для записи writeLock 2. добавление блокировок для чтения readLock()

--- UserRepository.java
+++ UserRepository.java
@@ -1,23 +1,41 @@
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class UserRepository {
     private List<User> users = new ArrayList<>();
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void addUser(User user) {
-        users.add(user);
+        lock.writeLock().lock();
+        try {
+            users.add(user);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public User getUser(int userId) {
-        return users.get(userId);
+        lock.readLock().lock();
+        try {
+            return users.get(userId);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    public List<User> getOrderedUsers(){
-        users.sort(new Comparator<User>() {
-            @Override
-            public int compare(User user1, User user2) {
-                return user1.compareTo(user2);
-            }
-        });
-        return users;
+    public List<User> getOrderedUsers() {
+        lock.readLock().lock();
+        try {
+            users.sort(new Comparator<User>() {
+                @Override
+                public int compare(User user1, User user2) {
+                    return user1.compareTo(user2);
+                }
+            });
+            return users;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
